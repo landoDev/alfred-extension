bfc24588cca76c9972424a9b34a2b15a96259d19
Landon Turner <lturner@teamworks.com>
2020-09-29 16:16:09 -0400

    add jquery

--- background.js
+++ background.js
@@ -1,20 +1,29 @@
-chrome.browserAction.onClicked.addListener(function(tab){
-    chrome.tabs.sendMessage(tab.id, "toggle");
-});
+// chrome.browserAction.onClicked.addListener(function(tab){
+//     chrome.tabs.sendMessage(tab.id,"toggle");
+// });
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // Send a message to the active tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    });
+  });
 
 // tutorial code
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log("The color is green.")
-    });
-});
+// chrome.runtime.onInstalled.addListener(function() {
+//     chrome.storage.sync.set({color: '#3aa757'}, function() {
+//         console.log("The color is green.")
+//     });
+// });
 
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
-    chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'developer.chrome.com'}
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-    }])
-})
+// chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
+//     chrome.declarativeContent.onPageChanged.addRules([{
+//         conditions: [new chrome.declarativeContent.PageStateMatcher({
+//             pageUrl: { hostEquals: 'developer.chrome.com'}
+//         })
+//         ],
+//             actions: [new chrome.declarativeContent.ShowPageAction()]
+//     }])
+// })
--- content-script.js
+++ content-script.js
@@ -6,9 +6,26 @@ chrome.runtime.onMessage.addListener(function(msg, sender){
     }
 })
 
+// style dash to be injected 
 let iframe = document.createElement('iframe');
 iframe.style.backgroundColor = "#36454f";
+iframe.style.color = "white";
 iframe.style.height = "100%";
 iframe.style.width = "0px";
+iframe.style.position = "fixed"
 iframe.style.display = "hidden";
+iframe.style.right = "0px";
+iframe.style.zIndex = "9000000000000000000";
+iframe.src = chrome.extension.getURL("popup.html")
 
+console.log(iframe)
+
+document.body.appendChild(iframe);
+
+function toggle(){
+    if(iframe.style.width == "0px"){
+        iframe.style.width = "400px";
+    } else {
+        iframe.style.width= "0px";
+    }
+}
